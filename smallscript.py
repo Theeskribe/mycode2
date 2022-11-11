42e7794dcb8c6403e263a185ed902286a8c6f748
theeskribe <example@example.com>
2022-11-11 16:33:45 +0000

    pylint testing code

---
+++ smallscript.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Author - theeskribe@gmail.com
+Description: A simple script for testing pylint usage. """
+
+def main():
+    """ main script function """
+
+    # string variable for pylint
+    my_note = "print this string"
+
+    # print the string
+    print(my_note)
+
+if __name__ == "__main__":
+    main()
